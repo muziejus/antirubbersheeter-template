20c47e4ba37c5ac541c90b42bda10a0856f9d2dd
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2022-03-02 13:35:09 -0500

    Step 4: add map as overlay.

--- js/map.js
+++ js/map.js
@@ -1,9 +1,23 @@
 // L is brought in as a global from the previous <script> tag in index.html.
+// same goes for data
 
 const center = L.latLng(0, 0);
 const zoom = 1;
+const minZoom = -5;
+const crs = L.CRS.Simple;
 
 const map = L.map("map", {
   center,
   zoom,
+  minZoom,
+  crs,
 });
+
+const bounds = [
+  [0, 0],
+  [data.height, data.width],
+];
+
+L.imageOverlay("braun-map-of-vilnius.jpg", bounds).addTo(map);
+
+map.fitBounds(bounds);
