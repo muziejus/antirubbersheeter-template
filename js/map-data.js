87b2b244903c629e670e35bccbc27a7bdf97b8d6
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2022-03-02 13:10:50 -0500

    Step 3: Add geocoded data file.

---
+++ js/map-data.js
@@ -0,0 +1,149 @@
+const data = {
+  width: 2661,
+  placesstring:
+    "Das hoche Slochs, Das Slochs, Die Jonge Königinne haus, Die Slochs kirche, S. Barbara kirch, Der Schatzmeister haus, De König stall, Der Lucerner, Das Deutſch haus, Das Trabanten haus, Die Wersope, Die bernardiner clost, Die Cancelerie, S. Iohan kirche, Das Wÿeweten haus, Das Biſhops haus, Der Grichë kirche, Das Rhathaus, Das Moskowiten hoff, Dis Schwartze munche, Das hoſpital, Die Keur kaſte, Die Sſatgoße, S Iohan goße, Der Cletken, Cletkens dor, Die dir garde, Uber Waſſer",
+  imgururl: "https://i.imgur.com/DbnFWAB.jpg",
+  height: 1954,
+  places: [
+    {
+      name: "Das hoche Slochs",
+      y: 1429,
+      x: 630,
+    },
+    {
+      name: "Das Slochs",
+      y: 1178,
+      x: 760,
+    },
+    {
+      name: "Die Jonge Königinne haus",
+      y: 1183,
+      x: 600,
+    },
+    {
+      name: "Die Slochs kirche",
+      y: 1054,
+      x: 561,
+    },
+    {
+      name: "S. Barbara kirch",
+      y: 1378,
+      x: 771,
+    },
+    {
+      name: "Der Schatzmeister haus",
+      y: 1142,
+      x: 997,
+    },
+    {
+      name: "De König stall",
+      y: 1281,
+      x: 818,
+    },
+    {
+      name: "Der Lucerner",
+      y: 1206,
+      x: 473,
+    },
+    {
+      name: "Das Deutſch haus",
+      y: 1275,
+      x: 649,
+    },
+    {
+      name: "Das Trabanten haus",
+      y: 1063,
+      x: 768,
+    },
+    {
+      name: "Die Wersope",
+      y: 1595,
+      x: 284,
+    },
+    {
+      name: "Die bernardiner clost",
+      y: 1356,
+      x: 994,
+    },
+    {
+      name: "Die Cancelerie",
+      y: 1212,
+      x: 1162,
+    },
+    {
+      name: "S. Iohan kirche",
+      y: 1144,
+      x: 1329,
+    },
+    {
+      name: "Das Wÿeweten haus",
+      y: 1090,
+      x: 1383,
+    },
+    {
+      name: "Das Biſhops haus",
+      y: 930,
+      x: 1287,
+    },
+    {
+      name: "Der Grichë kirche",
+      y: 1221,
+      x: 1414,
+    },
+    {
+      name: "Das Rhathaus",
+      y: 1224,
+      x: 1718,
+    },
+    {
+      name: "Das Moskowiten hoff",
+      y: 1292,
+      x: 1833,
+    },
+    {
+      name: "Dis Schwartze munche",
+      y: 1325,
+      x: 1931,
+    },
+    {
+      name: "Das hoſpital",
+      y: 933,
+      x: 1551,
+    },
+    {
+      name: "Die Keur kaſte",
+      y: 1221,
+      x: 1213,
+    },
+    {
+      name: "Die Sſatgoße",
+      y: 1225,
+      x: 1272,
+    },
+    {
+      name: "S Iohan goße",
+      y: 1024,
+      x: 1418,
+    },
+    {
+      name: "Der Cletken",
+      y: 1219,
+      x: 1582,
+    },
+    {
+      name: "Cletkens dor",
+      y: 781,
+      x: 2020,
+    },
+    {
+      name: "Die dir garde",
+      y: 1740,
+      x: 158,
+    },
+    {
+      name: "Uber Waſſer",
+      y: 995.2534473206099,
+      x: 80.18283237092032,
+    },
+  ],
+};
